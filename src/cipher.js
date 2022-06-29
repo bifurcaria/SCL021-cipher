a427877f95b5c2eab4d39bdaa93647626ba11ec4
bifurcaria <c.carlajm@gmail.com>
2022-06-29 13:24:16 -0400

    add cypher script

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,36 @@
-const cipher = {
+  // const cipher = {
   // ...
-};
+  // };
 
-export default cipher;
+//hay un input
+var input = 'HOLA';
+var toAscii = [];
+//pasalo a ascii
+for(let i = 0;
+  i < input.length;
+  i++){
+  toAscii.push(input.charCodeAt(i));
+}
+//definir variables
+var result = [];
+var fromText = true;
+//si cifras encode o si descifras decode
+if(fromText = true){
+for(let i = 0;
+  i < result.length;
+  i++)
+    {let cipherResult = [(i + offset)%26];
+    }
+    } else
+for(let i = 0;
+  i < result.length;
+  i++){
+  var cipherResult = [(i - offset)%26];}
+//pasalo a unicode
+var output = String.fromCharCode(cipherResult);
+//prueba
+console.log(toAscii);
+console.log(cipherResult);
+console.log(output);
+
+//export default cipher;
