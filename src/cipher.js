c0fdf1edfc4fc9dfb534ac60393140960cc7428a
bifurcaria <c.carlajm@gmail.com>
2022-06-30 13:32:33 -0400

    edit script, add stylesheet

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,8 @@
-  // const cipher = {
-  // ...
-  // };
+
+//const cipher = {
 
 //hay un input
-var input = 'HOLA';
+var input = document.getElementById('originalInput');
 var toAscii = [];
 //pasalo a ascii
 for(let i = 0;
@@ -13,24 +12,37 @@ for(let i = 0;
 }
 //definir variables
 var result = [];
-var fromText = true;
+var encodes = true;
+var offset = 3
+var cipherResult = [];
 //si cifras encode o si descifras decode
-if(fromText = true){
+if(encodes = true){
 for(let i = 0;
-  i < result.length;
+  i < toAscii.length;
   i++)
-    {let cipherResult = [(i + offset)%26];
+    { character = toAscii[i]
+      cipherChar = (((character - 65 + offset) % 26) + 65);
+      cipherResult.push(cipherChar)
     }
-    } else
-for(let i = 0;
-  i < result.length;
-  i++){
-  var cipherResult = [(i - offset)%26];}
+    } else {
+      for(let i = 0;
+        i < toAscii.length;
+        i++)
+          { character = toAscii[i]
+            cipherChar = (((character - 65 - offset) % 26) + 65);
+            cipherResult.push(cipherChar)
+          }
+    }
+
+
 //pasalo a unicode
-var output = String.fromCharCode(cipherResult);
+//var output = String.fromCharCode(cipherResult);
+var output = String.fromCharCode.apply(null, cipherResult)
 //prueba
 console.log(toAscii);
 console.log(cipherResult);
 console.log(output);
+//};
+
 
 //export default cipher;
